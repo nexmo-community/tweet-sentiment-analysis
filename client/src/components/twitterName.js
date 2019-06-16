336227c9d72bbb590b92e31265c74038bc77ca74
Lauren Lee <Laurenelee88@gmail.com>
2019-06-16 10:20:29 -0700

    updating react files performance

--- client/src/components/twitterName.js
+++ client/src/components/twitterName.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import unirest from 'unirest';
 
-class  App extends React.Component {
+
+class TwitterName extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -9,57 +10,25 @@ class  App extends React.Component {
       number: '',
       tweetContent: '',
       userName: ''
-    };
-    this.sendSMS = this.sendSMS.bind(this);
+  };
     this.sendUserName = this.sendUserName.bind(this);
-    this.calculateSentiment = this.calculateSentiment.bind(this);
     this.getTweet = this.getTweet.bind(this);
-    this.renderSentimentButton = this.renderSentimentButton.bind(this);
-    this.renderPhoneInput = this.renderPhoneInput.bind(this);
-    this.renderPhoneButton = this.renderPhoneButton.bind(this);
-  }
-
-  sendSMS() {
-    fetch('/sendSMS', {
-      method: 'post',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        number: this.state.number,
-        score: this.state.score,
-        tweetContent: this.state.tweetContent,
-        userName: this.state.userName
-      })
-    })
-    .then(function(res){ console.log(res) })
-    .catch(function(error){ console.log(error)});
+    this.calculateSentiment = this.calculateSentiment.bind(this);
+    this.sendSMS = this.sendSMS.bind(this);
   }
 
   sendUserName = (event) => {
     event.preventDefault();
+    const twitterHandle = this.state.userName
+    console.log('username in app.js', twitterHandle)
     fetch('/userName', {
       method: 'post',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({userName: this.state.userName})
+      body: JSON.stringify({userName: twitterHandle})
     })
     this.getTweet()
-  }
-
-  calculateSentiment = (event) => {
-    event.preventDefault();
-    unirest.post("https://microsoft-azure-text-analytics-v1.p.rapidapi.com/sentiment")
-    .header("X-RapidAPI-Host", "microsoft-azure-text-analytics-v1.p.rapidapi.com")
-    .header("X-RapidAPI-Key", "9e3f240e48mshb9079ed21621b98p17fd28jsn5d5db64d7dd9")
-    .header("Content-Type", "application/json")
-    .send({"documents":[{"language":"en","id":"string","text":this.state.tweetContent}]})
-    .end((result) => {
-      const newScore = result.body.documents[0].score
-      console.log("The score is:", newScore)
-      this.setState({ score: newScore })
-    });
   }
 
   getTweet = (event) => {
@@ -82,6 +51,20 @@ class  App extends React.Component {
     }
   }
 
+  calculateSentiment = (event) => {
+    event.preventDefault();
+    unirest.post("https://microsoft-azure-text-analytics-v1.p.rapidapi.com/sentiment")
+    .header("X-RapidAPI-Host", "microsoft-azure-text-analytics-v1.p.rapidapi.com")
+    .header("X-RapidAPI-Key", "9e3f240e48mshb9079ed21621b98p17fd28jsn5d5db64d7dd9")
+    .header("Content-Type", "application/json")
+    .send({"documents":[{"language":"en","id":"string","text":this.state.tweetContent}]})
+    .end((result) => {
+      const newScore = result.body.documents[0].score
+      console.log("The score is:", newScore)
+      this.setState({ score: newScore })
+    });
+  }
+
   renderPhoneInput() {
     if (this.state.score) {
       return (
@@ -91,7 +74,7 @@ class  App extends React.Component {
           <div className="ui input">
             Phone number:
             <input
-              placeholder="18005554444"
+              placeholder="18479623979"
               type="tel"
               value={this.state.number}
               onChange={(e) => this.setState({ number: e.target.value})}
@@ -101,6 +84,7 @@ class  App extends React.Component {
       );
     }
   }
+
   renderPhoneButton() {
     if (this.state.number) {
       return (
@@ -114,34 +98,50 @@ class  App extends React.Component {
     }
   }
 
+  sendSMS() {
+    fetch('/sendSMS', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        number: this.state.number,
+        score: this.state.score,
+        tweetContent: this.state.tweetContent,
+        userName: this.state.userName
+      })
+    })
+    .then(function(res){ console.log(res) })
+    .catch(function(error){ console.log(error)});
+  }
+
   render() {
     return (
-      <div className="ui container" style={{marginTop: '10px'}}>
-        <div className="ui input">
-          <input
-            placeholder="twitter name"
-            type="text"
-            value={this.state.userName}
-            onChange={(e) => this.setState({ userName: e.target.value})}
-          />
-          <button className="ui primary basic button" onClick={this.sendUserName}>
-            find most recent tweet
-          </button>
-        </div>
-
+      <div className="ui input">
+        <input
+          placeholder="twitter name"
+          type="text"
+          value={this.state.userName}
+          onChange={(e) => this.setState({ userName: e.target.value})}
+        />
+        <button className="ui primary basic button" onClick={this.sendUserName}>
+          find most recent tweet
+        </button>
         <div className="ui divider"></div>
 
         {this.state.tweetContent && this.state.tweetContent}
-
         <div className="ui divider"></div>
 
         {this.renderSentimentButton()}
+        <div className="ui divider"></div>
+
         {this.renderPhoneInput()}
+        <div className="ui divider"></div>
+
         {this.renderPhoneButton()}
 
       </div>
-    );
+    )
   }
 }
-
-export default App;
+export default TwitterName;
--- server.js
+++ server.js
@@ -1,26 +1,19 @@
 const dotenv = require('dotenv');
-dotenv.config();
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const Nexmo = require('nexmo');
 const Twitter = require('twitter');
 
+dotenv.config();
 const app = express();
-
-// Nexmo init
-const nexmo = new Nexmo({
-  apiKey: process.env.API_KEY,
-  apiSecret: process.env.API_SECRET
-}, {debug: true});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
 
 // Twitter init
 app.post('/userName', function(req, res) {
   userName = req.body.userName;
 
+  // twitter credentials
   app.get('/twitter', (req, res) => {
     const client = new Twitter({
       consumer_key: process.env.CONSUMER_KEY,
@@ -28,7 +21,7 @@ app.post('/userName', function(req, res) {
       access_token_key: process.env.ACCESS_TOKEN_KEY,
       access_token_secret: process.env.ACCESS_TOKEN_SECRET
     });
-
+    // read most recent tweet
     var username = {screen_name: userName };
     client.get('statuses/user_timeline', username, function(error, tweets, response) {
       if (!error) {
@@ -39,19 +32,37 @@ app.post('/userName', function(req, res) {
   });
 });
 
+// Nexmo credentials
+const nexmo = new Nexmo({
+  apiKey: process.env.API_KEY,
+  apiSecret: process.env.API_SECRET
+}, {debug: true});
+
+// send SMS via Nexmo
 app.post('/sendSMS', (req, res) => {
   res.send(req.body);
-  let text = req.body.text;
+  let score = req.body.score;
   let toNumber = req.body.number;
+  let tweet = req.body.tweetContent;
+  let userName = req.body.userName;
+  let scoreSign = '';
 
-  // Sending SMS via Nexmo
+  if (score > '.5') {
+    scoreSign = '✅'
+  } else if (score == '.5') {
+    scoreSign = '😐'
+  } else {
+    scoreSign = '👿'
+  }
+
+  //  Nexmo Messages API
   nexmo.channel.send(
     { type: 'sms', number: toNumber }, // To
     { type: 'sms', number: '12035809124' }, // From
     {
       content: {
         type: 'text',
-        text: `your most recent tweet's sentiment is: ${text}`,
+        text: `${userName}'s most recent tweet was ${tweet} and the sentiment score is: ${scoreSign}`,
       }
     },
     (err, data) => {
